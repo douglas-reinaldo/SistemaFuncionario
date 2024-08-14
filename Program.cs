4d6904da937ae7730db0de70b0561e24d22f2b64
Douglas <Douglas39510@gmail.com>
2024-08-14 16:16:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFuncionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool on = true;
+            SistemaFuncionarios sistema = new SistemaFuncionarios();
+            while (on)
+            {
+                int resposta = Menu();
+
+                switch (resposta)
+                {
+                    case 1:
+                        Console.WriteLine("------ Inserir Funcionário ------");
+                        Console.Write("Insira o nome do funcionário: ");
+                        string nome = Console.ReadLine();
+
+                        Console.Write("Insira a idade do funcionário: ");
+                        int idade = int.Parse(Console.ReadLine());
+
+                        Console.Write("Insira o cargo do funcionário: ");
+                        string cargo = Console.ReadLine();
+
+                        Console.Write("Insira o salário do funcionario: ");
+                        double salario = double.Parse(Console.ReadLine());
+
+                        Funcionario funcionario = new Funcionario(nome, idade, cargo, salario);
+                        sistema.AdicionarFuncionario(funcionario);
+
+                        break;
+
+                    case 2:
+                        Console.Write("Insira o nome do funcionário: ");
+                        string func = Console.ReadLine();
+                        sistema.EditarFuncionario(func);
+                        break;
+                    case 3:
+                        Console.Write("Insira o nome do funcionário que deseja excluir: ");
+                        string exNome = Console.ReadLine();
+                        sistema.ExcuirFuncionario(exNome);
+                        break;
+                    case 4:
+                        Console.WriteLine("Listando Funcionários: ");
+                        sistema.ListarFuncionarios();
+                        break;
+                    case 5:
+                        on = false;
+                        break;
+                }
+            }
+        }
+        static public int Menu()
+        {
+            Console.WriteLine("------------- Menú -------------");
+            Console.WriteLine("1 - Adicionar Funcionário\n2 - Editar informações do Funcionário\n3 - Excluir Funcionário\n4 - Listar todos funcionários\n5 - Sair do Programa");
+            int inteiro = int.Parse(Console.ReadLine());
+            return inteiro;
+        }
+    }
+}
---
+++ SistemaFuncionarios.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFuncionarios
+{
+    internal class SistemaFuncionarios
+    {
+        public List<Funcionario> sistema = new List<Funcionario>();
+
+
+        public void AdicionarFuncionario(Funcionario func)
+        {
+            Console.WriteLine("Adicionando funcionário ao sistema..");
+            sistema.Add(func);
+            Console.WriteLine("Funcionário adicionado ao sistema com sucesso!");
+        }
+
+        public void EditarFuncionario(string nome)
+        {
+            Funcionario func = sistema.Find(funcionario => funcionario.Nome == nome);
+            if (func != null)
+            {
+                int escolha = Opcao();
+
+                switch (escolha)
+                {
+                    case 1:
+                        Console.WriteLine("Insira o novo nome: ");
+                        string novoNome = Console.ReadLine();
+                        func.Nome = novoNome;
+                        break;
+                    case 2:
+                        Console.WriteLine("Insira a nova idade: ");
+                        int novaIdade = int.Parse(Console.ReadLine());
+                        func.Idade = novaIdade;
+                        break;
+                    case 3:
+                        Console.WriteLine("Insira o novo cargo: ");
+                        string novoCargo = Console.ReadLine();
+                        func.Cargo = novoCargo;
+                        break;
+                    case 4:
+                        Console.WriteLine("Insira o novo nome: ");
+                        double novoSalario = double.Parse(Console.ReadLine());
+                        func.Salario = novoSalario;
+                        break;
+                }
+                Console.WriteLine("Edição feita com sucesso!\nDados alterados: ");
+                Console.WriteLine(func);
+            }
+            else
+            {
+                Console.WriteLine("Funcionário não encontrado!");
+            }
+        }
+
+        public void ExcuirFuncionario(string nome)
+        {
+            Funcionario func = sistema.Find(funcionario => funcionario.Nome == nome);
+            if (func != null)
+            {
+                Console.WriteLine("Funcionário Encontrado!\nExcluindo Funcionário..");
+                sistema.Remove(func);
+                Console.WriteLine("Funcionário Exluido do sistema com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Funcionário não encontrado! ");
+            }
+        }
+
+        public int Opcao()
+        {
+            Console.WriteLine("Insira a opção para editar as informações do funcionário: ");
+            Console.WriteLine("1 - Nome\n2 - Idade\n3 - Cargo\n4 - Salario");
+            int escolha = int.Parse(Console.ReadLine());
+            return escolha;
+        }
+
+        public void ListarFuncionarios()
+        {
+            if (sistema.Count == 0)
+            {
+                Console.WriteLine("Sem funcionários cadastrados! ");
+            }
+            else
+            {
+                foreach (Funcionario func in sistema)
+                {
+                    Console.WriteLine(func);
+                }
+            }
+        }
+    }
+}
